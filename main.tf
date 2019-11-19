0e78d0f33b83c20fc2a1297b530dad4cf3abdccb
Soumyajit <soumyda1@in.ibm.com>
2019-11-19 12:15:06 +0530

    maps

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ variable "zones" {
   type = "list"
   default = ["us-east-1a", "us-east-1b"]
 }
+
+variable "images" {
+  type = "map"
+
+  default = {
+    us-east-1 = "image-1234"
+    us-west-2 = "image-4567"
+  }
+}
 output "Workvalues" {
-  value = "${var.zones}"
+  value = "${var.images}"
 }
